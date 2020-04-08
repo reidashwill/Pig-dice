5ce5698d7eaeff376e92af9a00f046732df59a5a
William Pratt <willpratt78@gmail.com>
2020-04-08 11:12:41 -0700

    change amount of constructors, and have rollDie function work

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 // Business logic for Player----------------------------
 
 function Player(){
-  this.turnScore = [];
-  this.totalScore = [];
+  this.turnScore = 0;
+  this.totalScore = 0;
   this.currentId = 0;
 }
 
@@ -19,5 +19,22 @@ function Score(turnScore, totalScore) {
 }
 
 Score.prototype.rollDie = function(){
+  Math.floor(Math.random() * 6) + 1;
   
 }
+
+Score.prototype.addTurnScore = function(rollScore, turnScore) {
+  var newTurnScore = (rollScore + turnScore)
+}
+
+// UI logic ---------------------------------------------
+
+$(document).ready(function(){
+  $("#roll-button").click(function(event){
+    event.preventDefault();
+    var turnRoll = rollDie();
+    console.log(turnRoll);
+    
+  })
+  
+})
