1fbddd4f70f8ed509ea6a3e73bd49ea4ed80691f
William Pratt <willpratt78@gmail.com>
2020-04-08 14:33:23 -0700

    target array for playerOne

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,48 @@
 // Business logic for Player----------------------------
 
 function Player(){
-  this.turnScore = 0;
-  this.totalScore = 0;
+  this.turnScore = [];
+  this.totalScore = [];
   this.currentId = 0;
 }
 
-Player.prototype.assignId = function() {
-  this.currentId +=1;
-  return this.currentId
+
+Player.prototype.rollPush = function(rollScore) {
+  console.log(rollScore);
+this.turnScore.push(rollScore);
 }
 
-// Business logic for Score------------------------------
 
-function Score(turnScore, totalScore) {
-  this.turnScore = turnScore;
-  this.totalScore = totalScore;
+
+// Player.prototype.assignId = function() {
+//   this.currentId +=1;
+//   return this.currentId
+// }
+
+// Business logic------------------------------
+
+var rollDie = function(){
+return Math.floor(Math.random() * 6) + 1;
+
 }
+// UI logic---------------------------------------------
+var playerOne = new Player;
+console.log(playerOne.turnScore)
+var playerTwo = new Player (turnScore,totalScore,1);
+// playerTwo.assignId()
+console.log(playerOne)
+console.log(playerTwo)
 
-Score.prototype.rollDie = function(){
-  Math.floor(Math.random() * 6) + 1;
-  
+for (var i= 0; i < 2; i++){
+
 }
-
-Score.prototype.addTurnScore = function(rollScore, turnScore) {
-  var newTurnScore = (rollScore + turnScore)
-}
-
-// UI logic ---------------------------------------------
 
 $(document).ready(function(){
+  // attatchContactListeners();
   $("#roll-button").click(function(event){
     event.preventDefault();
-    var turnRoll = rollDie();
-    console.log(turnRoll);
-    
+    var rollScore = rollDie();
+    console.log(rollScore)
+    console.log(playerOne)
   })
-  
 })
