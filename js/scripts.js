f22502cf98dba1b0f993061c52faaed291a5a276
William Pratt <willpratt78@gmail.com>
2020-04-08 15:38:39 -0700

    troubleshoot logic

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,6 @@ function Player(){
   this.currentId = 0;
 }
 
-
-Player.prototype.rollPush = function(rollScore) {
-  console.log(rollScore);
-this.turnScore.push(rollScore);
-}
-
-
-
-// Player.prototype.assignId = function() {
-//   this.currentId +=1;
-//   return this.currentId
-// }
-
 // Business logic------------------------------
 
 var rollDie = function(){
@@ -28,21 +15,25 @@ return Math.floor(Math.random() * 6) + 1;
 // UI logic---------------------------------------------
 var playerOne = new Player;
 console.log(playerOne.turnScore)
-var playerTwo = new Player (turnScore,totalScore,1);
+var playerTwo = new Player;
 // playerTwo.assignId()
 console.log(playerOne)
 console.log(playerTwo)
-
-for (var i= 0; i < 2; i++){
-
-}
 
 $(document).ready(function(){
   // attatchContactListeners();
   $("#roll-button").click(function(event){
     event.preventDefault();
     var rollScore = rollDie();
-    console.log(rollScore)
-    console.log(playerOne)
+    playerOne.turnScore.push(rollScore);
+    console.log(rollScore);
+    console.log(playerOne);
+    $('#output-number').append(' ' + rollScore);
+  })
+  $("#hold-button").click(function(event){
+    event.preventDefault();
+    var totalScore = Player.turnScore
+    console.log(totalScore)
+    console.log(playerOne.totalScore);
   })
 })
